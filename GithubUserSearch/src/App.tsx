05102c10fd1b92d8d7278b4643c862445460fbf1
habul17 <abulhassanca.dev@gmail.com>
2026-09-12 11:19:53 +0530

    feat: controlled search input with useState

--- GithubUserSearch/src/App.tsx
+++ GithubUserSearch/src/App.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react"
 
 function App() {
 
+  const [username, setUsername] = useState("");
+
   return <div>
     <h1>Github User Search</h1>
+    <input value={username} onChange={(e) => setUsername(e.target.value)} />
+    <p>{username}</p>
   </div>
 
 }
