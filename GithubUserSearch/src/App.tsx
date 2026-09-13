103877ccaa03dbe356444d118a7f0445cbdf4395
habul17 <abulhassanca.dev@gmail.com>
2026-09-13 10:07:47 +0530

    feat: fetch github user on username change

--- GithubUserSearch/src/App.tsx
+++ GithubUserSearch/src/App.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 function App() {
-
   const [username, setUsername] = useState("");
+
+
+  useEffect(() => {
+    if (username.trim() === "") return;
+    axios.get(`https://api.github.com/users/${username}`)
+      .then(response => console.log(response.data))
+  }, [username])
+
+  console.log("App ran");
+
 
   return <div>
     <h1>Github User Search</h1>
